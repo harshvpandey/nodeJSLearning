b87ea1a95b25ee9507a8b91463accbb9f6a643f5
Harshvardhan Pandey <hvpandey2000@gmail.com>
2025-04-24 16:47:13 +0530

    hoisting in js

---
+++ javascript/hoisting.js
@@ -0,0 +1,16 @@
+console.log("Harsh")
+getName();
+console.log(getName)
+
+function getName(){
+    console.log("Harsh is good")
+}
+
+var anotherName=()=>{
+    console.log("this will be undefined as now it is an arrow function and is treated as varaible")
+}
+console.log(x)
+
+var x=0;
+
+var y,z,a,b,c;
